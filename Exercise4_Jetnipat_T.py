94989ab9b4ef1eadead664ac4d32081b49c1720c
armjetnipat123 <50545594+armjetnipat123@users.noreply.github.com>
2019-05-14 17:54:01 +0700

    Add files via upload

---
+++ Exercise4_Jetnipat_T.py
@@ -0,0 +1,10 @@
+F = 86
+G = 73
+I = 49
+C = 91
+
+print("------- Your Score ------")
+print("Foundation English               :",F,"Score")
+print("General Business                 :",G,"Score")
+print("Introduction to Computer Systems :",I,"Score")
+print("Computer Programming             :",C,"Score")
