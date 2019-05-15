7b2066a9f124683c9a04d0116144b9de689ade51
armjetnipat123 <50545594+armjetnipat123@users.noreply.github.com>
2019-05-15 17:35:00 +0700

    Add files via upload

---
+++ Exercise5_1_Jetnipat_T.py
@@ -0,0 +1,15 @@
+print("-- Calculator--")
+x = int(input("Number 1 :"))
+y = int(input("Number 2 :"))
+print("-- Your Answer--")
+
+P = x + y
+Mi = x - y
+Mu = x * y
+D = int(x /y)
+
+print(x,"+",y,"=",P)
+print(x,"-",y,"=",Mi)
+print(x,"*",y,"=",Mu)
+print(x,"/",y,"=",D)
+print("-- Thank You For Use --")
