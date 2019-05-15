63c9e5c371f1f6360148499577dd2d5260ae5955
armjetnipat123 <50545594+armjetnipat123@users.noreply.github.com>
2019-05-15 15:56:18 +0700

    Update Exercise4_Jetnipat_T.py

--- Exercise4_Jetnipat_T.py
+++ Exercise4_Jetnipat_T.py
@@ -3,8 +3,9 @@ G = 73
 I = 49
 C = 91
 
-print("------- Your Score ------")
+print("----------------- Your Score ----------------")
 print("Foundation English               :",F,"Score")
 print("General Business                 :",G,"Score")
 print("Introduction to Computer Systems :",I,"Score")
 print("Computer Programming             :",C,"Score")
+print("----------------- Good Luck----------------- ")
