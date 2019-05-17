d87bcc87153b8a8adea037284db6a8ab24cf7822
armjetnipat123 <50545594+armjetnipat123@users.noreply.github.com>
2019-05-17 18:14:44 +0700

    Update Exercise8_Jetnipat_T.py

--- Exercise8_Jetnipat_T.py
+++ Exercise8_Jetnipat_T.py
@@ -18,30 +18,35 @@ if username == usernameInput and password == passwordInput:
         Price1 = 5000
         print("   ")
         print("You Bought", Music1,"Guitars", "Total is :", Music1 * Price1, "THB")
+        print("Pls Contac Our Sell To Get Your Price")
         print("--------------- Thx For Use Our Shop ---------------")
     if userselect == 2:
         Music2 = int(input("How Much Do You Want Bass? :"))
         Price2 = 6500
         print("   ")
         print("You Bought", Music2, "Bass", "Total is :", Music2 * Price2, "THB")
+        print("Pls Contac Our Sell To Get Your Price")
         print("--------------- Thx For Use Our Shop ---------------")
     if userselect == 3:
         Music3 = int(input("How Much Do You Want Bass? :"))
         Price3 = 6500
         print("   ")
         print("You Bought", Music3, "Keyboard", "Total is :", Music3 * Price3, "THB")
+        print("Pls Contac Our Sell To Get Your Price")
         print("--------------- Thx For Use Our Shop ---------------")
     if userselect == 4:
         Music4 = int(input("How Much Do You Want Violine? :"))
         Price4 = 20000
         print("   ")
         print("You Bought", Music4, "Violine", "Total is :", Music4 * Price4, "THB")
+        print("Pls Contac Our Sell To Get Your Price")
         print("--------------- Thx For Use Our Shop ---------------")
     if userselect == 5:
         Music5 = int(input("How Much Do You Want Drum? :"))
         Price5 = 30000
         print("   ")
         print("You Bought", Music5, "Drum", "Total is :", Music5 * Price5, "THB")
+        print("Pls Contac Our Sell To Get Your Price")
         print("--------------- Thx For Use Our Shop ---------------")
 else:
     print("Pls check your username or password!!")
