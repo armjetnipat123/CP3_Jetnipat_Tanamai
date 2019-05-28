b1ef4b8b864f2d88ee7ba9d8e732fca517e7bbda
armjetnipat123 <50545594+armjetnipat123@users.noreply.github.com>
2019-05-28 16:53:09 +0700

    Add files via upload

---
+++ L50.py
@@ -0,0 +1,12 @@
+def PlusNumber(x,y):
+    print(x+y)
+PlusNumber(20,20)
+def MinusNumber(x,y):
+    print(x-y)
+MinusNumber(40,10)
+def MultiNumber(x,y):
+    print(x*y)
+MultiNumber(5,4)
+def DiviNumber(x,y):
+    print(x/y)
+DiviNumber(20,2)
